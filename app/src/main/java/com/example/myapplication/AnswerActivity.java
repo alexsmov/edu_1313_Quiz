6a458c68adbae4c18ce3c032f34cfe8546c0aa18
alexsmov <92463762+alexsmov@users.noreply.github.com>
2021-11-18 15:59:15 +0300

    Завершение разработки первого приложения

--- app/src/main/java/com/example/myapplication/AnswerActivity.java
+++ app/src/main/java/com/example/myapplication/AnswerActivity.java
@@ -5,20 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AnswerActivity extends AppCompatActivity {
+
     private boolean isAnswerTrue;
     private TextView answerTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
-        // Получаем дополнение
-        isAnswerTrue = getIntent().getBooleanExtra("answer", true);
+
+        isAnswerTrue = getIntent().getBooleanExtra("answer",true);
         answerTextView = findViewById(R.id.answerTextView);
-        answerTextView.setText(isAnswerTrue?R.string.yes:R.string.no);
-       /* if(isAnswerTrue)
-            answerTextView.setText(R.string.yes);
+        answerTextView.setText(isAnswerTrue?R.string.yas:R.string.no);
+
+        /* if (isAnswerTrue)
+            answerTextView.setText(R.string.yas);
         else
             answerTextView.setText(R.string.no);*/
+
     }
 }
